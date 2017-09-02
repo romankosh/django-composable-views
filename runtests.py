106476be64530a1b1d18acaf1d5d55696e68ce39
Alex Tkachenko <preusx.dev@gmail.com>
2017-09-02 14:47:41 +0300

    Small changes to a runtest sute.

--- runtests.py
+++ runtests.py
@@ -17,12 +17,9 @@ def configure(nose_args=None):
                 'APP_DIRS': True,
             }],
             INSTALLED_APPS=[
-                'django.contrib.contenttypes',
-                'django.contrib.auth',
                 'composable_views',
-                'tests',
             ],
-            ROOT_URLCONF='tests.urls',
+            ROOT_URLCONF='composable_views.tests.urls',
             NOSE_ARGS=nose_args
         )
 
@@ -32,7 +29,7 @@ def runtests(*test_args):
     runner = NoseTestSuiteRunner()
 
     if not test_args:
-        test_args = ['tests']
+        test_args = ['composable_views']
     num_failures = runner.run_tests(test_args)
     if num_failures:
         sys.exit(num_failures)
@@ -40,14 +37,18 @@ def runtests(*test_args):
 
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option('--with-coverage', dest='coverage', default=False,
-                      action='store_true')
-    parser.add_option('--with-xunit', dest='xunit', default=False,
-                      action='store_true')
-    parser.add_option('--with-spec', dest='with_spec', default=False,
-                      action='store_true')
-    parser.add_option('--pdb', dest='pdb', default=False,
-                      action='store_true')
+    parser.add_option(
+        '--with-coverage', dest='coverage', default=False, action='store_true'
+    )
+    parser.add_option(
+        '--with-xunit', dest='xunit', default=False, action='store_true'
+    )
+    parser.add_option(
+        '--with-spec', dest='with_spec', default=False, action='store_true'
+    )
+    parser.add_option(
+        '--pdb', dest='pdb', default=False, action='store_true'
+    )
     options, args = parser.parse_args()
 
     nose_args = []
@@ -59,6 +60,10 @@ if __name__ == '__main__':
         # why the coverage trigger uses '--with-coverage' and why we don't need
         # to explicitly include it here.
         nose_args.extend([
-            '--cover-package=extra_views', '--cover-branch', '--cover-html', '--cover-html-dir=htmlcov'])
+            '--cover-package=extra_views',
+            '--cover-branch',
+            '--cover-html',
+            '--cover-html-dir=htmlcov'
+        ])
     configure(nose_args)
     runtests()
