63319d9f68e518647030b84fe459f894920b25c8
Alex Tkachenko <preusx.dev@gmail.com>
2017-09-02 14:45:40 +0300

    Added install and test requirements.

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
+import os
 from setuptools import setup
+
+
+def requirements_getter(filename):
+    result = []
+
+    with open(filename) as f:
+        requirements = map(lambda x: x.strip(), f.readlines())
+
+        for requirement in requirements:
+            if not requirement.startswith('-r'):
+                result.append(requirement)
+                continue
+
+            result += requirements_getter(
+                os.path.join(os.path.dirname(filename), requirement[2:].strip())
+            )
+
+    return result
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
 
 setup(
     name='django-composable-views',
     version='0.1.0',
     url='https://gitlab.com/preusx/django-composable-views',
-    install_requires=[
-        'Django >=1.9',
-    ],
+    install_requires=requirements_getter(
+        os.path.join(here, 'requirements.txt')
+    ),
+    tests_require=requirements_getter(
+        os.path.join(here, 'requirements/test.txt')
+    ),
     description="Composable class-based views for Django",
     long_description=open('README.rst', 'r').read(),
     license="MIT",
